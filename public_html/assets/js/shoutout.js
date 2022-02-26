0031a30eb939780fc8475f46529ad5d8be649b72
Andrew <andrew.c.depue@gmail.com>
2022-02-26 04:00:41 -0600

    .

--- public_html/assets/js/shoutout.js
+++ public_html/assets/js/shoutout.js
@@ -170,7 +170,7 @@ $(document).ready(function () {
                     "' type='video/mp4'></video>"
                 ).appendTo("#container");
 
-                document.getElementById("clip").onplaying = function () {
+                object.onplay = function () {
                   document.getElementById("clip").remove();
                 };
 
